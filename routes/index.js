968574880950c174fb3a133d1977e4cd069ae86f
osamamohsen <osamamohsen1994@gmail.com>
2017-06-05 23:39:54 +0200

    retrieve data from mongo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var Product = require('../models/product');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+  Product.find(function (err,docs) {
+    var productChunks = [];
+    var limit_width = 3;
+    for(var i=0; i<docs.length ; i+= limit_width)
+      productChunks.push(docs.slice(i,i+limit_width));
+    res.render('shop/index', { title: 'Shopping Cart' , products: productChunks});
+  });
 });
 
 module.exports = router;
